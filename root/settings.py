76107ddf9aefbc3bd2026f01dec8be29456e3b18
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-09-13 17:52:35 +0500

    language tilni joyida o'zgartiradugan qildim

--- root/settings.py
+++ root/settings.py
@@ -145,7 +145,8 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
-
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'us-east-1'
 customColorPalette = [
     {
         'color': 'hsl(4, 90%, 58%)',
